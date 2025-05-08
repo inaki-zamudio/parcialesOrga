8d3e0dcacc0dd0414c022dba6a82a82f54c1f46b
inaki <inakizamudio13@gmail.com>
2025-05-08 20:47:03 -0300

    Ej a y b andando, c falta arreglar

--- 1P/2c2024-r1/ej1/ej1.asm
+++ 1P/2c2024-r1/ej1/ej1.asm
@@ -18,14 +18,14 @@ COLUMNAS EQU 255
 ; Funciones a implementar:
 ;   - optimizar
 global EJERCICIO_1A_HECHO
-EJERCICIO_1A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1A_HECHO: db 1 ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - contarCombustibleAsignado
 global EJERCICIO_1B_HECHO
-EJERCICIO_1B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1B_HECHO: db 1 ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1C como hecho (`true`) o pendiente (`false`).
 ;
@@ -72,36 +72,37 @@ optimizar:
 	xor rbx, rbx ; int i = 0
 .loop:
 	cmp rbx, FILAS * COLUMNAS ; ¿ i == 255^2 ?
-	jge .fin
+	je .fin
 
-	cmp r12, r13 ; if (mapa[i][j] == compartida)
+	cmp [r12], r13 ; if (mapa[i][j] == compartida)
 	je .nextIteration
 
-	cmp r12, 0 ; if (mapa[i][j] == NULL)
+	cmp qword [r12], 0 ; if (mapa[i][j] == NULL)
 	je .nextIteration
 
-	mov rdi, r12 ; preparo mapa[i][j] para call
+	mov rdi, [r12] ; preparo mapa[i][j] para call
 	call r14 ; eax <-- hash_actual = fun_hash(mapa[i][j])
 
 	cmp eax, r15d ; if(hash_compartida != hash_actual)
 	jne .nextIteration
 
-	dec byte [r12 + ATTACKUNIT_REFERENCES] ; mapa[i][j]->references--
+	mov r8, [r12]
+	dec byte [r8 + ATTACKUNIT_REFERENCES] ; mapa[i][j]->references--
 
-	cmp byte [r12 + ATTACKUNIT_REFERENCES], 0
-	je .liberar
+	cmp byte [r8 + ATTACKUNIT_REFERENCES], 0 ; if (mapa[i][j]->references == 0)
+	jne .reasignar
+
+	mov rdi, [r12] ; preparo mapa[i][j] para call
+	call free
+	
+.reasignar:
+	mov [r12], r13 ; mapa[i][j] = compartida
+	inc byte [r13 + ATTACKUNIT_REFERENCES] ; compartida->references++
 
 .nextIteration:
 	add r12, 8 ; mapa[(i*j)+1]
 	inc rbx ; (i++)
 	jmp .loop
-
-.liberar:
-	mov rdi, r12 ; preparo mapa[i][j] para call
-	call free
-	mov [r12], r13 ; mapa[i][j] = compartida
-	inc byte [r13 + ATTACKUNIT_REFERENCES] ; compartida->references++
-	jmp .nextIteration
 
 .fin:
 	; epilogo
@@ -118,12 +119,118 @@ global contarCombustibleAsignado
 contarCombustibleAsignado:
 	; rdi = mapa_t           mapa
 	; rsi = uint16_t*        fun_combustible(char*)
+	
+	; prologo
+	push rbp
+	mov rbp, rsp
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbx
+	sub rsp, 8
+	
+	mov r12, rdi ; mapa
+	mov r13, rsi ; fun_combustible
+
+	xor r15d, r15d ; res
+	xor rbx, rbx ; i
+
+.loop:
+	cmp rbx, FILAS * COLUMNAS ; i == FILAS * COLUMNAS?
+	je .fin
+
+	cmp qword [r12], 0 ; if(mapa[i][j] == NULL)
+	je .nextIteration
+
+	mov r14, [r12]
+	mov rdi, [r12 + ATTACKUNIT_CLASE]
+	call r13 ; ax <-- fun_combustible(mapa[i][j]->clase)
+
+	mov r8w, word [r14 + ATTACKUNIT_COMBUSTIBLE] ; comb_base
+	sub r8w, ax ; combustible - comb_base
+	movzx r8d, r8w
+	add r15d, r8d ; res += mapa[i][j]->combustible - comb_base
+
+.nextIteration:
+	add r12, 8
+	inc rbx
+	jmp .loop
+
+.fin:
+	; epilogo
+	mov eax, r15d
+	add rsp, 8
+	pop rbx
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbp
 	ret
 	
 global modificarUnidad
 modificarUnidad:
 	; rdi = mapa_t           mapa
 	; sil  = uint8_t          x
-	; dil  = uint8_t          y
+	; dl  = uint8_t          y
 	; rcx = void*            fun_modificar(attackunit_t*)
+
+	; prologo
+	push rbp
+	mov rbp, rsp
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbx
+	sub rsp, 8
+
+	mov r12, rdi
+	mov r13b, sil
+	mov r14b, dl
+	mov r15, rcx
+
+	; r12 + 8*(255*x + y) = mapa[x][y]
+	mov r8, COLUMNAS
+	mul r8, r13b
+	add r8, r14b
+	imul r8, 8
+	add r8, r12
+	mov rbx, r8 ; mapa[x][y]
+
+	cmp rbx, 0 ; mapa[x][y] == NULL ?
+	je .fin
+
+	mov rdi, rbx
+	mov r9, [rbx + ATTACKUNIT_REFERENCES] ; mapa[x][y]->references == 1
+	cmp r9, 1
+	je .fun_modificar
+
+	mov rdi, ATTACKUNIT_SIZE
+	call malloc ; rax = attackunit_t* nueva_unidad
+	mov r12, rax
+	mov r8, [rbx] ; *mapa[x][y]
+	mov [r12], r8 ; *nueva_unidad = *mapa[x][y]
+	dec [rbx + ATTACKUNIT_REFERENCES] ; mapa[x][y]->references--
+	mov [r12 + ATTACKUNIT_REFERENCES], byte 1 ; nueva_unidad->references = 1
+	mov rdi, r12
+	call r15
+
+
+
+.fun_modificar:
+	call r15
+	jmp .fin
+
+
+.fin:
+	; epilogo
+	add rsp, 8
+	pop rbx
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbp
 	ret
--- 1P/2c2024-r1/ej1/ej1.c
+++ 1P/2c2024-r1/ej1/ej1.c
@@ -10,7 +10,7 @@
  * Funciones a implementar:
  *   - es_indice_ordenado
  */
-bool EJERCICIO_1A_HECHO = true;
+bool EJERCICIO_1A_HECHO = false;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -26,7 +26,7 @@ bool EJERCICIO_1B_HECHO = false;
  * Funciones a implementar:
  *   - modificarUnidad
  */
-bool EJERCICIO_1C_HECHO = false;
+bool EJERCICIO_1C_HECHO = true;
 
 /**
  * OPCIONAL: implementar en C
@@ -64,12 +64,34 @@ void optimizar(mapa_t mapa, attackunit_t* compartida, uint32_t (*fun_hash)(attac
  * OPCIONAL: implementar en C
  */
 uint32_t contarCombustibleAsignado(mapa_t mapa, uint16_t (*fun_combustible)(char*)) {
-    
+    uint32_t res = 0;
+    for (int i = 0; i < 255; i++) {
+        for (int j = 0; j < 255; j++) {
+            if (mapa[i][j] == NULL) {
+                continue;
+            }
+            uint16_t comb_base = fun_combustible(mapa[i][j]->clase);
+            res += mapa[i][j]->combustible - comb_base;
+        }
+    }
+    return res;
 }
 
 /**
  * OPCIONAL: implementar en C
  */
 void modificarUnidad(mapa_t mapa, uint8_t x, uint8_t y, void (*fun_modificar)(attackunit_t*)) {
-   
+    if (mapa[x][y] == NULL){
+        return;
+    }
+    if (mapa[x][y]->references == 1){
+    fun_modificar(mapa[x][y]);
+   } else {
+    attackunit_t* nueva_unidad = malloc(sizeof(attackunit_t));
+    *nueva_unidad = *mapa[x][y];
+    mapa[x][y]->references--;
+    nueva_unidad->references = 1;
+    fun_modificar(nueva_unidad);
+    mapa[x][y] = nueva_unidad;
+   }
 }
